05a55894ca8144575d3ef6098234938f44700bbc
Tobias Powelske <FirimaElda@gmail.com>
2021-12-01 19:51:55 +0100

    Completed second task of first day of AoC_2021.

--- day_01/sonar_sweep.py
+++ day_01/sonar_sweep.py
@@ -5,9 +5,30 @@ if __name__ == '__main__':
         reader = csv.reader(fd)
         previous_depth = None
         number_of_increases = 0
+        list_of_depths = []
         for row in reader:
             current_depth = int(row[0])
+            list_of_depths.append(current_depth)
             if previous_depth is not None and previous_depth < current_depth:
                 number_of_increases += 1
             previous_depth = current_depth
-        print(number_of_increases)
+        print('The depth has increased ' + str(number_of_increases) + ' number of times!')
+        sliding_a_index = 0
+        sliding_b_index = 1
+        sliding_c_index = 2
+        previous_window_sum = None
+        number_of_window_increases = 0
+        for current_index in range(len(list_of_depths)):
+            sliding_a_index = current_index
+            sliding_b_index = current_index + 1
+            sliding_c_index = current_index + 2
+            try:
+                current_window_sum = list_of_depths[sliding_a_index] + list_of_depths[sliding_b_index] + list_of_depths[
+                    sliding_c_index]
+                if previous_window_sum is not None and previous_window_sum < current_window_sum:
+                    number_of_window_increases += 1
+                previous_window_sum = current_window_sum
+            except:
+                print('No complete window possible! Abort.')
+                break
+        print(number_of_window_increases)
